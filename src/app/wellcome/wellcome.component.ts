4e8e2926b57f6ca63b7a3324e3f251a5b7c2bdd9
Jaswanth <jaswanth.d304@gmail.com>
2023-03-12 23:36:23 -0400

    Wellcome page updated

--- src/app/wellcome/wellcome.component.ts
+++ src/app/wellcome/wellcome.component.ts
@@ -10,21 +10,7 @@ import { DataserviceService, responceIntreface } from '../service/dataservice.se
 export class WellcomeComponent {
   
   constructor(private routeractive: ActivatedRoute,
-    private _dataservice: DataserviceService) { }
+    ) { }
   name = sessionStorage.getItem('authenticate')
-  Spring1="jaswanthkrishna"
-  Spring=''
   
-
-  getSpringData() {
-    this._dataservice.dataServiceWithpathVariable(this.Spring1).subscribe(
-     data =>  this.Spring=data.msg
-    );
-
-  }dataServiceWithpathVariable(){
-    this._dataservice.dataServiceWithpathVariable(this.Spring1).subscribe(
-      data =>  this.Spring=data.msg
-    )
-  }
-
 }
